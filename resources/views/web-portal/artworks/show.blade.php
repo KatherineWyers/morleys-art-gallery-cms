29c58235d2e005940a7856263552aabe969671bd
Katherine Wyers <contact@katherinewyers.com>
2018-03-14 23:28:49 +0000

    Add contact button to artworks show-page Add the model pop-up window to the artworks show-page
    Add the Skype-call button that launches the skype application
    
    Add the Skype-chat button that launches the skype chat-window.

--- resources/views/web-portal/artworks/show.blade.php
+++ resources/views/web-portal/artworks/show.blade.php
@@ -29,6 +29,16 @@
                         <li>Price: £{{ $artwork->price }}</li>
                     </ul>
                     <p>{!! nl2br(e($artwork->desc_1)) !!}</p>
+
+                    <h2>Would you like more details?</h2>
+                    <p>Contact us by phone or via Skype to speak to a member of our team and arrange a private viewing</p>
+
+
+                    <!-- Button trigger modal -->
+                    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+                      Call, LiveChat or Skype
+                    </button>
+
                     <div class="row">
                         <div class="col-xs-6 col-sm-4">
                             <a href="/artworks/{{ $artwork->id }}/1"><img src="/img/artworks/{{ $artwork->img_1 }}" class="img-responsive"></a>
@@ -44,3 +54,61 @@
             </div>
         </div>
     </section>
+
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Contact Us about "{{ $artwork->title }}"</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <div class="row">
+            <div class="col-xs-12 col-sm-6">
+                <h2>Contact Us to book an Appointment</h2>
+                <p>
+                    Phone: <br />+44 2049 12345678
+                </p>
+
+                <p>
+                    1 Main Street, <br />Straford-upon-Avon, <br />AV12 DX3, <br />United Kingdom
+                </p>
+            </div>
+            <div class="col-xs-12 col-sm-6">
+                <h2>Call or LiveChat on Skype</h2>
+                <p>
+                    <script type="text/javascript" src="https://secure.skypeassets.com/i/scom/js/skype-uri.js"></script>
+                    <div id="SkypeButton_Call_live:contact_48907_1">
+                     <script type="text/javascript">
+                     Skype.ui({
+                     "name": "chat",
+                     "element": "SkypeButton_Call_live:contact_48907_1",
+                     "participants": ["live:contact_48907"],
+                     "imageSize": 32
+                     });
+                     </script>
+                    </div>
+
+                    <div id="SkypeButton_Call_live:contact_48907_1">
+                     <script type="text/javascript">
+                     Skype.ui({
+                     "name": "call",
+                     "element": "SkypeButton_Call_live:contact_48907_1",
+                     "participants": ["live:contact_48907"],
+                     "imageSize": 32
+                     });
+                     </script>
+                    </div>
+                </p>
+            </div>
+        </div>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>
+</div>
