cceacfa66b65163f69e79e77790ee94f05adf1b2
Katherine Wyers <contact@katherinewyers.com>
2018-04-19 17:45:55 +0100

    Hotfix Bug#4: Homepage featured artworks displaying hidden artworks Add a visible filter to the featured_artworks query in PagesController.

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -22,7 +22,7 @@ class PagesController extends Controller
      */
     public function frontpage(Request $request)
     {        
-        $featured_artworks=Artwork::orderBy('created_at', 'desc')->limit(6)->get();
+        $featured_artworks=Artwork::visible()->orderBy('created_at', 'desc')->limit(6)->get();
         $current_exhibitions=Exhibition::current()->limit(2)->get();
         $latest_news_articles=NewsArticle::orderBy('created_at', 'desc')->limit(2)->get();
         $response = response()->view('web-portal.frontpage', compact('featured_artworks', 'current_exhibitions', 'latest_news_articles'));
