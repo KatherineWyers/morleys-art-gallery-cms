9eb4bff79795c40e0827a294bbe268e08eba2242
Katherine Wyers <contact@katherinewyers.com>
2018-04-26 14:09:18 +0100

    Hotfix#30: Add background image to standardpage in accessible css Add the large gallery background image to the accessible style sheet.
    Add a contrasting background to the text on gallery and contact pages so the
    text is legible over the new background image.

--- resources/views/web-portal/contact.blade.php
+++ resources/views/web-portal/contact.blade.php
@@ -7,19 +7,24 @@
                     <img src="img/logo-315x249.png" class="img-responsive">
                 </div>  
                 <div class="col-xs-12 col-md-6">
-                    <h1>Contact us</p>
-                    <h2>Address</h2>
-                    <ul>
-                        <li>37 Marlborough Court,</li>
-                        <li>Stratford-upon-Avon,</li>
-                        <li>EI1 6NJ</li>
-                        <li>Phone: +44 1234 5678</li>
-                    </ul>
-                    <h2>Opening Times</h2>
-                    <ul>
-                        <li>Mon-Fri: 9am - 5pm</li>
-                        <li>Sat, Sun and Bank Holidays: 11am - 3pm</li>
-                    </ul>
+                    <div class="panel panel-default">
+                        <div class="panel-heading"><h1>Contact us</h1>
+                        </div>
+                        <div class="panel-body">
+                        <h2>Address</h2>
+                        <ul>
+                            <li>37 Marlborough Court,</li>
+                            <li>Stratford-upon-Avon,</li>
+                            <li>EI1 6NJ</li>
+                            <li>Phone: +44 1234 5678</li>
+                        </ul>
+                        <h2>Opening Times</h2>
+                        <ul>
+                            <li>Mon-Fri: 9am - 5pm</li>
+                            <li>Sat, Sun and Bank Holidays: 11am - 3pm</li>
+                        </ul>
+                        </div>
+                    </div> 
                 </div>  
             </div>
         </div>
--- resources/views/web-portal/gallery.blade.php
+++ resources/views/web-portal/gallery.blade.php
@@ -7,11 +7,16 @@
                     <img src="img/logo-315x249.png" class="img-responsive">
                 </div>  
                 <div class="col-xs-12 col-md-6">
-                    <h1>Morley's Art Gallery</h1>
-                    <h2>Straford-upon-Avon</h2>
-                    <p>The gallery was founded in 1976. Lorem ipsum dolor sit amet, illum ignota ius eu. Molestie oportere mea in. Everti numquam sea no. Aeterno vidisse meliore ne vix, eum ubique cotidieque ut, per in explicari vituperatoribus. At eum option deseruisse, nam blandit apeirian no.</p>
-                    <p>Has te dicta tollit. Ferri errem pro cu. Ad appetere reprehendunt vim, facilisis delicatissimi ex vim, simul diceret mea ex. Vel falli maiestatis et. Patrioque efficiendi id sit, te qui utamur feugiat expetenda.</p>
-                    <p>Ut liber facilisi assueverit quo, pri an cibo minim sadipscing, usu ne vivendum accusata. Sea partem definiebas cu, ei sumo veniam imperdiet vel, agam aeterno tamquam ea eam. Sit dolorem ponderum ne, sea fugit consectetuer ei. Dicant tritani liberavisse ea vis. Ad his purto euismod, elit mundi his an.</p>
+                    <div class="panel panel-default">
+                        <div class="panel-heading"><h1>Morley's Art Gallery</h1>
+                        </div>
+                        <div class="panel-body">
+                            <h2>Straford-upon-Avon</h2>
+                            <p>The gallery was founded in 1976. Lorem ipsum dolor sit amet, illum ignota ius eu. Molestie oportere mea in. Everti numquam sea no. Aeterno vidisse meliore ne vix, eum ubique cotidieque ut, per in explicari vituperatoribus. At eum option deseruisse, nam blandit apeirian no.</p>
+                            <p>Has te dicta tollit. Ferri errem pro cu. Ad appetere reprehendunt vim, facilisis delicatissimi ex vim, simul diceret mea ex. Vel falli maiestatis et. Patrioque efficiendi id sit, te qui utamur feugiat expetenda.</p>
+                            <p>Ut liber facilisi assueverit quo, pri an cibo minim sadipscing, usu ne vivendum accusata. Sea partem definiebas cu, ei sumo veniam imperdiet vel, agam aeterno tamquam ea eam. Sit dolorem ponderum ne, sea fugit consectetuer ei. Dicant tritani liberavisse ea vis. Ad his purto euismod, elit mundi his an.</p>
+                        </div>
+                    </div> 
                 </div>  
             </div>
         </div>
